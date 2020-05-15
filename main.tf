ac79d72cc4a50b250b9cae655617873e24b3edb2
Migara Ekanayake <mekanayake@paloaltonetworks.com>
2020-05-15 13:27:00 +0100

    first commit

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-variable "firewalls" {
-  default = {
-    firewall-a-tmp = {
-
-      interfaces = [
-        { name = "eth0", subnet = "test", index = 0, public_ip = true },
-        { name = "eth1", subnet = "test", index = 1, public_ip = true },
-        { name = "eth2", subnet = "test", index = 2 }
-      ]
-    },
-    firewall-b-tmp = {
-      interfaces = [
-        { name = "eth0", subnet = "test", index = 0 },
-        { name = "eth1", subnet = "test", index = 1 },
-        { name = "eth2", subnet = "test", index = 2 }
-      ]
-    }
-  }
-}
-
 locals {
   interfaces = flatten([
     for fw_name, firewall in var.firewalls : [
@@ -28,21 +8,28 @@ locals {
   ])
 }
 
-variable "tags" {
-  default = {}
+data "aws_ami" "this" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["PA-VM-AWS-${var.fw_version}*"]
+  }
+
+  filter {
+    name   = "product-code"
+    values = ["${var.fw_product_map[var.fw_product]}"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["aws-marketplace"]
 }
 
-
-output "test" {
-  # value = local.interfaces
-  value = { for i in local.interfaces : "${i.fw_name}-${i.name}" => i }
-}
-
-# module.vpc.subnets["Central-Mgmt-2a"].id, secrity_groups = []
-# module.vpc.subnets["Central-Mgmt-2a"].id, secrity_groups = []
-# module.vpc.subnets["Central-Mgmt-2a"].id, secrity_groups = []
-
-resource "aws_network_interface" "eni-management" {
+resource "aws_network_interface" "this" {
   for_each          = { for i in local.interfaces : "${i.fw_name}-${i.name}" => i }
   subnet_id         = each.value.subnet
   security_groups   = each.value.secrity_groups
@@ -51,105 +38,41 @@ resource "aws_network_interface" "eni-management" {
   tags = merge(var.tags, { Name = each.key })
 }
 
-# resource "aws_eip" "eip-management" {
-#   for_each          = var.management_subnet
-#   vpc               = true
-#   network_interface = aws_network_interface.eni-management[each.key].id
+resource "aws_eip" "this" {
+  for_each = { for i in local.interfaces :
+    "${i.fw_name}-${i.name}" => i
+    if lookup(i, "public_ip", null) != null ? true : false
+  }
+  vpc               = true
+  network_interface = aws_network_interface.this[each.key].id
 
-#   tags = {
-#     Name = "eip_${each.key}_management"
-#   }
-# }
+  tags = merge(var.tags, { Name = "${each.key}-eip" })
+}
 
-# resource "aws_eip" "eip-untrust" {
-#   for_each          = var.untrust_subnet
-#   vpc               = true
-#   network_interface = aws_network_interface.eni-untrust[each.key].id
+resource "aws_instance" "this" {
+  for_each                             = var.firewalls
+  disable_api_termination              = false
+  instance_initiated_shutdown_behavior = "stop"
+  ebs_optimized                        = true
+  ami                                  = var.custom_ami != null ? var.custom_ami : data.aws_ami.this.id
+  instance_type                        = var.instance_type
+  key_name                             = var.key_name
+  user_data                            = var.user_data
+  monitoring                           = false
 
-#   tags = {
-#     Name = "eip_${each.key}_untrust"
-#   }
-# }
+  root_block_device {
+    delete_on_termination = "true"
+  }
 
-# # output "test" {
-# #   value = aws_network_interface.eni-management
-# # }
+  dynamic "network_interface" {
+    for_each = each.value.interfaces
 
-# resource "aws_instance" "instance-ngfw" {
-#   disable_api_termination              = false
-#   instance_initiated_shutdown_behavior = "stop"
+    content {
+      device_index         = network_interface.value.index
+      network_interface_id = aws_network_interface.this["${each.key}-${network_interface.value.name}"].id
+    }
 
-#   ebs_optimized = true
-#   ami           = var.custom_ami
-#   instance_type = "m4.xlarge"
-#   key_name      = "aws-moj"
+  }
 
-#   monitoring = false
-
-#   root_block_device {
-#     delete_on_termination = "true"
-#   }
-#   network_interface {
-#     device_index         = 1
-#     network_interface_id = aws_network_interface.eni-management["security-Mgmt-a"].id
-#   }
-
-#   network_interface {
-#     device_index         = 0
-#     network_interface_id = aws_network_interface.eni-untrust["security-Pub-a"].id
-#   }
-
-#   network_interface {
-#     device_index         = 2
-#     network_interface_id = aws_network_interface.eni-trust["security-Priv-a"].id
-#   }
-
-#   tags = {
-#     Name = "GP POC MOJ FW 1"
-#   }
-# }
-
-# resource "aws_instance" "instance-ngfw-2" {
-#   disable_api_termination              = false
-#   instance_initiated_shutdown_behavior = "stop"
-
-#   ebs_optimized = true
-#   ami           = var.custom_ami
-#   instance_type = "m4.xlarge"
-#   key_name      = "aws-moj"
-
-#   monitoring = false
-
-#   root_block_device {
-#     delete_on_termination = "true"
-#   }
-#   network_interface {
-#     device_index         = 1
-#     network_interface_id = aws_network_interface.eni-management["security-Mgmt-b"].id
-#   }
-
-#   network_interface {
-#     device_index         = 0
-#     network_interface_id = aws_network_interface.eni-untrust["security-Pub-b"].id
-#   }
-
-#   network_interface {
-#     device_index         = 2
-#     network_interface_id = aws_network_interface.eni-trust["security-Priv-b"].id
-#   }
-
-#   tags = {
-#     Name = "GP POC MOJ FW 2"
-#   }
-# }
-
-# # output "eip_untrust" {
-# #   value = "${aws_eip.eip-untrust.public_ip}"
-# # }
-
-# # output "eip_mgmt" {
-# #   value = "${aws_eip.eip-management.public_ip}"
-# # }
-# # output "trust_eni_id" {
-# #   value = "${aws_network_interface.eni-trust.id}"
-# # }
+  tags = merge(var.tags, { Name = each.key })
+}
---
+++ variables.tf
@@ -0,0 +1,46 @@
+
+variable "tags" {
+  description = "Optional Tags to apply to all resources"
+  type        = map(any)
+  default     = {}
+}
+
+variable "firewalls" {
+  description = "map of firewalls to create"
+  type        = map(any)
+  default     = {}
+}
+
+variable "fw_version" {
+  description = "Firewall version to deploy."
+  type        = string
+  default     = "9.1.2"
+}
+
+variable "fw_product" {
+  description = "Type of VM-Series to deploy.  Can be one of 'byol', 'bundle-1', 'bundle-2'."
+  default     = "byol"
+}
+
+variable "fw_product_map" {
+  type = map(string)
+
+  default = {
+    byol     = "6njl1pau431dv1qxipg63mvah"
+    bundle-1 = "6kxdw3bbmdeda3o6i1ggqt4km"
+    bundle-2 = "806j2of0qy5osgjjixq9gqc6g"
+  }
+}
+
+variable "instance_type" {
+  description = "Instance type for FW"
+  default     = "m4.xlarge"
+}
+
+variable "key_name" {
+  default = null
+}
+
+variable "user_data" {
+  default = null
+}
