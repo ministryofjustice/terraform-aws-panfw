38ed62527cf64c0b4fb10a8df343bc72af56cee8
Migara Ekanayake <mekanayake@paloaltonetworks.com>
2020-05-15 15:20:31 +0100

    add output

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,13 @@ output "pafw" {
 }
 
 output "primary_public_ip" {
-  value = { for name, fw in aws_instance.this : name => fw.public_ip }
+  value = { for name, fw in aws_instance.this : name => lookup(fw, "public_ip", "") }
+}
+
+output "network_interfaces" {
+  value = aws_network_interface.this
+}
+
+output "elastic_ips" {
+  value = aws_eip.this
 }
