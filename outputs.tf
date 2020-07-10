5b55c5303b54689442bbed859be60c277f40d3e0
Migara Ekanayake <mekanayake@paloaltonetworks.com>
2020-07-10 10:01:46 +0100

    remove output primary_public_ip, user should use elastic_ips to retrieve the elastic IPs

--- outputs.tf
+++ outputs.tf
@@ -2,15 +2,10 @@ output "pafw" {
   value = aws_instance.this
 }
 
-output "primary_public_ip" {
-  value = { for name, fw in aws_instance.this : name => lookup(fw, "public_ip", "") }
-}
-
 output "network_interfaces" {
   value = aws_network_interface.this
 }
 
 output "elastic_ips" {
-  # value = { for e in matchkeys(values(aws_eip.this), keys(aws_eip.this), keys(aws_network_interface_attachment.this)) : e.tags.FW_Name => e }
   value = aws_eip.this
 }
