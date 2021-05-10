96fd1590e064f6effc9d726a5280cfcc1b94dc88
Hasitha Ubeywarna <hasitha@local>
2021-05-10 10:20:16 +0100

    TF module enhancements

--- variables.tf
+++ variables.tf
@@ -1,20 +1,14 @@
 
-variable "tags" {
-  description = "Optional tags to apply to all resources"
-  type        = map(any)
-  default     = {}
-}
-
-variable "firewalls" {
-  description = "List of firewalls to create"
-  type        = list(any)
-  default     = []
-}
-
-variable "fw_version" {
-  description = "Firewall version to deploy"
+variable "name" {
+  description = "Name of the VM-Series virtual machine."
+  default     = "fw00"
   type        = string
-  default     = "9.1.2"
+}
+
+variable "panos_version" {
+  description = "PAN-OS version of the firewall to deploy"
+  type        = string
+  default     = "9.1.9"
 }
 
 variable "fw_product" {
@@ -24,7 +18,7 @@ variable "fw_product" {
 
 variable "fw_product_map" {
   description = "Firewall product codes"
-  type = map(string)
+  type        = map(string)
 
   default = {
     byol     = "6njl1pau431dv1qxipg63mvah"
@@ -33,24 +27,20 @@ variable "fw_product_map" {
   }
 }
 
+variable "custom_ami_id" {
+  description = "Custom AMI id to use instead of the usual fw_product_map"
+  type        = string
+  default     = null
+}
+
 variable "instance_type" {
   description = "EC2 instance type for firewall"
   type        = string
   default     = "m5.xlarge"
 }
 
-variable "key_name" {
-  description = "AWS SSH key name"
-  type        = string
-  default     = null
-}
-
-variable "user_data" {
-  default = null
-}
-
-variable "custom_ami" {
-  description = "Custom AMI id to use instead of the usual fw_product_map"
+variable "ssh_key_name" {
+  description = "AWS EC2 key pair name"
   type        = string
   default     = null
 }
@@ -61,7 +51,47 @@ variable "iam_instance_profile" {
   default     = null
 }
 
-variable "public_ipv4_pool" {
-  description = "EC2 IPv4 address pool identifier"
-  default     = "amazon"
+variable "user_data" {
+  default = null
+}
+
+variable "interfaces" {
+  description = <<-EOF
+  List of the network interface specifications.
+  The first should be the Management network interface, which does not participate in data filtering.
+  The remaining ones are the dataplane interfaces.
+  - `name`: (Required|string) Name tag for the ENI.
+  - `description`: (Optional|string) A descriptive name for the ENI.
+  - `subnet_id`: (Required|string) Subnet ID to create the ENI in.
+  - `private_ip_address`: (Optional|string) Private IP to assign to the ENI. If not set, dynamic allocation is used.
+  - `eip_allocation_id`: (Optional|string) Associate an existing EIP to the ENI.
+  - `create_public_ip`: (Optional|bool) Whether to create a public IP for the ENI. Default false.
+  - `public_ipv4_pool`: (Optional|string) EC2 IPv4 address pool identifier. 
+  - `source_dest_check`: (Optional|bool) Whether to enable source destination checking for the ENI. Default true.
+  Example:
+  ```
+  interfaces =[
+    {
+      name: "mgmt"
+      subnet_id: subnet-00000000000000001
+      create_public_ip: true
+    },
+    {
+      name: "public"
+      subnet_id: subnet-00000000000000002
+      create_public_ip: true
+    },
+    {
+      name: "private"
+      subnet_id: subnet-00000000000000003
+    },
+  ]
+  ```
+  EOF
+}
+
+variable "tags" {
+  description = "A map of tags to be associated with the resources created."
+  default     = {}
+  type        = map(any)
 }
